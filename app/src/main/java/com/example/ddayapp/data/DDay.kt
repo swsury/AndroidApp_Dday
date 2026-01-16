05e377bfa83dcd9e4a094be36cb26c021f57e295
SIHYUN PARK <155856066+swsury@users.noreply.github.com>
2026-01-16 18:36:20 +0900

    주석 작업

--- app/src/main/java/com/example/ddayapp/data/DDay.kt
+++ app/src/main/java/com/example/ddayapp/data/DDay.kt
@@ -1,15 +1,17 @@
 package com.example.ddayapp.data
 
+// 애플리케이션에서 사용되는 개별 디데이 정보를 담는 데이터 모델
+// 각 디데이별로 적용
 data class DDay(
-    val id: Long,
-    val title: String,
-    val labelTitle: String,
-    val date: String,
-    val color: String,
+    val id: Long, // 디데이 ID (식별용 기본키)
+    val title: String, // 디데이 이름
+    val labelTitle: String, // 디데이 분류 라벨
+    val date: String, // 목표 날짜
+    val color: String, // 디데이 색상
 
-    val excludePublicHolidays:  Boolean,
-    val excludeCustomDays: Boolean,
-    val excludedWeekdays: Set<Int> = emptySet(),
+    val excludePublicHolidays: Boolean, // 공휴일 제외 여부
+    val excludeCustomDays: Boolean, // 안식일 제외 여부
+    val excludedWeekdays: Set<Int> = emptySet(), // 제외 요일, 기본값은 비어 있는 세트로 모든 요일을 포함하여 계산
 
-    val order: Int = 0
+    val order: Int = 0 // 오름차순 정렬, 숫자가 작을수록 목록 위쪽에 표시
 )
--- app/src/main/java/com/example/ddayapp/data/Holiday.kt
+++ app/src/main/java/com/example/ddayapp/data/Holiday.kt
@@ -1,6 +1,7 @@
 package com.example.ddayapp.data
 
+// 애플리케이션에서 사용되는 개별 휴일 정보를 담는 데이터 모델
 data class Holiday(
-    val date: String, // Format: "yyyy-MM-dd"
-    val name: String = ""
+    val date: String, // 휴일 날짜, format : "yyyy-MM-dd"
+    val name: String = "" // 휴일 이름
 )
